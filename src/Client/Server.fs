e852f65d77f311b3eea0a852ab385b115cc78bd0
Jiří Landsman <landsman@cngroup.dk>
2020-07-20 15:35:36 +0200

    removed base url

--- src/Client/Server.fs
+++ src/Client/Server.fs
@@ -5,12 +5,9 @@ open Fable.Remoting.Client
 
 open Cookbook.Shared.Auth
 
-[<Emit("config.baseUrl")>]
-let baseUrl : string = jsNative
 
     /// A proxy you can use to talk to server directly
 let authService : AuthService =
     Remoting.createApi()
     |> Remoting.withRouteBuilder Route.builder
-    |> Remoting.withBaseUrl baseUrl
     |> Remoting.buildProxy<AuthService>
