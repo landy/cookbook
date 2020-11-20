8306ac030182baaac3fae2d8f22b522963efcfea
Jiří Landsman <landsman@cngroup.dk>
2020-11-20 20:08:28 +0100

    changes

--- src/Libraries/Password.fs
+++ src/Libraries/Password.fs
@@ -16,7 +16,7 @@ let iterationCount = 10000
 
 
 [<MethodImpl(MethodImplOptions.NoInlining ||| MethodImplOptions.NoOptimization)>]
-let private bytesAreEqual (a:byte []) (b:byte []) =
+let bytesAreEqual (a:byte []) (b:byte []) =
     if a = null && b = null then
         true
     elif a = null || b = null || a.Length <> b.Length then
@@ -28,17 +28,17 @@ let private bytesAreEqual (a:byte []) (b:byte []) =
         areEqual
 
 
-let private writeNetworkByteOrder (buffer:byte []) (offset:int) (value:uint32) =
+let writeNetworkByteOrder (buffer:byte []) (offset:int) (value:uint32) =
     buffer.[(offset + 0)] <- (value >>> 24) |> byte
     buffer.[(offset + 1)] <- (value >>> 16) |> byte
     buffer.[(offset + 2)] <- (value >>> 8) |> byte
     buffer.[(offset + 3)] <- (value >>> 0) |> byte
     ()
 
-let private readNetworkByteOrder (buffer:byte []) offset =
-    (buffer.[offset + 0] <<< 24 |> uint32)
-    ||| (buffer.[offset + 1] <<< 16 |> uint32)
-    ||| (buffer.[offset + 2] <<< 8 |> uint32)
+let readNetworkByteOrder (buffer:byte []) offset =
+    (buffer.[offset + 0] |> uint32 <<< 24)
+    ||| (buffer.[offset + 1] |> uint32 <<< 16)
+    ||| (buffer.[offset + 2] |> uint32 <<< 8)
     ||| (buffer.[offset + 3] |> uint32)
 
 let createHash password =
@@ -59,16 +59,17 @@ let createHash password =
     |> Convert.ToBase64String
 
 
-let verifyHashedPassword hashedPassword password =
+let verifyHashedPassword hash password =
+    let hashedPassword = hash |> Convert.FromBase64String
     let prf : KeyDerivationPrf = readNetworkByteOrder hashedPassword 1 |> int |> enum
     let iterCount = readNetworkByteOrder hashedPassword 5 |> int
     let saltLength = readNetworkByteOrder hashedPassword 9 |> int
 
-    let salt = Array.zeroCreate<Byte> saltLength
-    Buffer.BlockCopy(hashedPassword, 13, salt, 0, salt.Length)
+    let currentSalt = Array.zeroCreate<Byte> saltLength
+    Buffer.BlockCopy(hashedPassword, 13, currentSalt, 0, currentSalt.Length)
 
-    let subKeyLength = hashedPassword.Length - 13 - salt.Length
+    let subKeyLength = hashedPassword.Length - 13 - currentSalt.Length
     let expectedSubKey = Array.zeroCreate<Byte> subKeyLength
-    Buffer.BlockCopy(hashedPassword, 13 + salt.Length, expectedSubKey, 0, expectedSubKey.Length)
-    let actualSubKey = KeyDerivation.Pbkdf2(password, salt, prf, iterCount, subKeyLength)
+    Buffer.BlockCopy(hashedPassword, 13 + currentSalt.Length, expectedSubKey, 0, expectedSubKey.Length)
+    let actualSubKey = KeyDerivation.Pbkdf2(password, currentSalt, prf, iterCount, subKeyLength)
     bytesAreEqual actualSubKey expectedSubKey
