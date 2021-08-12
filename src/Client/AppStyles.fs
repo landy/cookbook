a2a1c67afe38d141e1a20b84c1f78083c35f3c3c
Jiří Landsman <landsman@cngroup.dk>
2021-08-12 14:34:52 +0200

    optimize css imports

--- src/Client/AppStyles.fs
+++ src/Client/AppStyles.fs
@@ -6,4 +6,6 @@ open Fable.Core.JsInterop
 let initStyles () =
     console.log("init styles")
     importSideEffects "./styles/style.scss"
+    importSideEffects "bulma"
+    importSideEffects "@fortawesome/fontawesome-free/css/all.min.css"
     ()
